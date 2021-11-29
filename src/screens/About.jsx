6726b905d2374872edad75312362b7d7905e8334
Kamyab Rouhifar <krouhifar@myseneca.ca>
2021-11-29 00:56:30 -0500

    updated the application

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -1,10 +1,50 @@
 import React from "react";
+import { Card, Image, Button } from "react-bootstrap";
 
 function About() {
   return (
-    <div>
-      <h2>Hello About</h2>
-    </div>
+    <Card className="m-auto mt-4 border border-4 text-dark">
+      <Card.Body>
+        <Card.Title>About me</Card.Title>
+        <Image
+          src="https://avatars.githubusercontent.com/u/54970142?v=4"
+          thumbnail
+          style={{ maxWidth: "50%" }}
+        />
+        <Card.Subtitle className="mt-1 text-muted">
+          Kamyab Rouhifar
+        </Card.Subtitle>
+        <Card.Text>
+          <p>
+            I am web developer for more than 2 years of web application
+            developing.
+          </p>
+          <em>This is an React application for tracking real-time covid-19</em>
+        </Card.Text>
+        <div className="d-flex justify-content-around col-6">
+          <Card.Link
+            href="https://github.com/krouhifar"
+            className="text-white"
+            style={{ textDecoration: "none" }}
+          >
+            <Button variant="primary" className="text-white">
+              <i class="fab fa-github-square fs-2"></i>
+              <div>Github</div>
+            </Button>
+          </Card.Link>
+          <Card.Link
+            href="https://www.kamyabrouhifar.ca"
+            className="text-white"
+            style={{ textDecoration: "none" }}
+          >
+            <Button variant="primary">
+              <i class="fab fa-wordpress-simple fs-2"></i>
+              <div>Portfolio</div>
+            </Button>
+          </Card.Link>
+        </div>
+      </Card.Body>
+    </Card>
   );
 }
 
--- src/screens/Contact.jsx
+++ src/screens/Contact.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-
+import { Card } from "react-bootstrap";
 function Contact() {
   return (
-    <div>
-      <h2>Hello Contact</h2>
-    </div>
+    <Card className="m-auto mt-4 border border-4 text-dark w-50">
+      <Card.Body>
+        <Card.Title className="text-center">Contact Information</Card.Title>
+        <Card.Text>
+          <p>
+            Phone : <code>437-984-5385</code>
+          </p>
+          <p>
+            E-mail : <code>karouhifar@gmail.com</code>
+          </p>
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 }
 
